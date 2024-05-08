c2aa4c019cb385d676b70006624cc7efdd1b9421
James Attard <jamesat@mcast.org.mt>
2024-05-08 11:18:58 +0200

    Added more comments. Updated the handleAddTodo to use mutable arrays

--- src/pages/Todo.tsx
+++ src/pages/Todo.tsx
@@ -10,16 +10,19 @@ import {
 import { useState } from "react";
 
 const Todo: React.FC = () => {
-  //////////////////
+  ///////////////////
   // State Variables
-  //////////////////
+  ///////////////////
 
+  // Array that stored a list of todo items as strings
   const [todoList, setTodoList] = useState<string[]>([]);
+
+  // The new todo item input by the user inside <IonInput />
   const [newTodo, setNewTodo] = useState("");
 
-  ////////////
+  //////////////
   // Functions
-  ////////////
+  /////////////
 
   // Function will save the user's input to a state var
   const handleUserInput = (event: any) => {
@@ -29,39 +32,45 @@ const Todo: React.FC = () => {
   // Function will add the user's input to the todoList state array
   const handleAddTodo = () => {
     // Create a temp array to add the new todo item
-    const tempArray = todoList;
+    const tempArray = [];
+    // Copy the state array todoList to tempArray
+    for (let i = 0; i < todoList.length; i++) {
+      tempArray.push(todoList[i]);
+    }
+    // Push the new todo to the end of the array
     tempArray.push(newTodo);
-    setTodoList(tempArray); // Update the todo list with the new item
-    setNewTodo(""); // Refresh the user's input field
+    setTodoList(tempArray); // Update the state variable Todo List with the latest version of tempArray
+    setNewTodo(""); // Reset the user's input field
 
-    console.log("Todo list: ", todoList);
+    console.log("todoList: ", todoList);
   };
 
   const handleDelete = (indexToDelete: number) => {
-    // Create a temp array to store only the todo items I want to keep
+    // Create a temp array to store only the todo items that I want to keep
     // (and skip the one I want to delete)
     const tempArray = [];
     for (let i = 0; i < todoList.length; i++) {
-      if (i !== indexToDelete) {
+      // If the index is not the same as the the element where the button was clicked,
+      // copy the element to tempArray
+      if (i != indexToDelete) {
         tempArray.push(todoList[i]);
       }
     }
-
-    // Update the todoList state array with the final values of the tempArray
-    // In other words, the todoList state array will contain all items except the one we just deleted
+    // Update the todoList state array with the most recent elements of the tempArray
     setTodoList(tempArray);
+
+    console.log("todoList: ", todoList);
   };
 
-  // Function will render the formatted version of the Todo list in an array called formattedArray
-  // 1) Initialize an empty array formattedArray
-  // 2) Loop through the state array todoList
-  // 3) For each element inside todoList, get the string todoList[i] (the todo item)
-  // 4) Inside the formattedArray, create an <IonItem> component with the previous string
-  // inside an <IonLabel> and followed by a delete <IonButton>
-  // 5) Return this formattedArray to the user
+  // Function will render a formatted version of todoList inside an
+  // array called formattedArray.
   const renderTodoList = () => {
+    // Create a temp array that will store the Ionic components formatted todoList
     const formattedArray = [];
+    // Loop the todoList array
     for (let i = 0; i < todoList.length; i++) {
+      // For each todoList item, add some Ionic component formatting
+      // and save to formattedArray
       formattedArray.push(
         <IonItem key={i}>
           <IonLabel>{todoList[i]}</IonLabel>
@@ -69,6 +78,8 @@ const Todo: React.FC = () => {
         </IonItem>
       );
     }
+
+    // Return the formattedArray to the user
     return formattedArray;
   };
 
