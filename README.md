54efb3e348a56e7b5218fccbade79780b57dd296
James Attard <jamesat@mcast.org.mt>
2024-05-08 08:21:47 +0200

    Added README file

---
+++ README.md
@@ -0,0 +1,5 @@
+
+# Todo App (Ionic-React)
+
+Todo App developed in Ionic-React. This is the codebase for the final app in the Introduction to Mobile App Development unit intended for my students.
+
--- src/pages/Todo.tsx
+++ src/pages/Todo.tsx
@@ -1,65 +1,68 @@
-import { IonButton, IonContent, IonInput, IonItem, IonLabel, IonList, IonPage } from "@ionic/react"
+import {
+  IonButton,
+  IonContent,
+  IonInput,
+  IonItem,
+  IonLabel,
+  IonList,
+  IonPage,
+} from "@ionic/react";
 import { useState } from "react";
 
 const Todo: React.FC = () => {
+  //////////////////
+  // State Variables
+  //////////////////
 
-    //////////////////
-    // State Variables
-    //////////////////
+  const [todoList, setTodoList] = useState<string[]>([]);
+  const [newTodo, setNewTodo] = useState("");
 
-    const [todoList, setTodoList] = useState<string[]>([]);
-    const [newTodo, setNewTodo] = useState("");
+  ////////////
+  // Functions
+  ////////////
 
+  // Function will save the user's input to a state var
+  const handleUserInput = (event: any) => {
+    setNewTodo(event.target.value);
+  };
 
-    ////////////
-    // Functions
-    ////////////
-    
-    // Function will save the user's input to a state var
-    const handleUserInput = (event: any) => {
-        setNewTodo(event.target.value);
+  // Function will add the user's input to the todoList state array
+  const handleAddTodo = () => {
+    const todoListArray = todoList;
+    todoListArray.push(newTodo);
+    setTodoList(todoListArray); // Update the todo list with the new item
+    setNewTodo(""); // Refresh the user's input field
+
+    console.log("Todo list: ", todoList);
+  };
+
+  const renderTodoList = () => {
+    const formattedArray = [];
+    for (let i = 0; i < todoList.length; i++) {
+      formattedArray.push(
+        <IonItem key={i}>
+          <IonLabel>{todoList[i]}</IonLabel>
+          <IonButton>Delete</IonButton>
+        </IonItem>
+      );
     }
+    return formattedArray;
+  };
 
-
-    // Function will add the user's input to the todoList state array
-    const handleAddTodo = () => {
-        const todoListArray = todoList;
-        todoListArray.push(newTodo);
-        setTodoList(todoListArray);
-        setNewTodo(""); // Refresh the user's input field
-
-        console.log("Todo list: ", todoList)
-    }
-
-    const renderTodoList = () => {
-        const formattedArray = [];
-        for (let i = 0; i < todoList.length; i++) {
-            formattedArray.push (
-                <IonItem key={i}>
-                    <IonLabel>{todoList[i]}</IonLabel>
-                    <IonButton>Delete</IonButton>
-                </IonItem>
-            )
-        }
-        return formattedArray;
-    }
-
-    return (
-        <IonPage>
-            <IonContent>
-                <h1>Todo App</h1>
-                <IonList>
-                    {renderTodoList()}
-                </IonList>
-                <IonInput 
-                    placeholder="Enter new Todo"
-                    value={newTodo}
-                    onIonInput={handleUserInput}
-                />
-                <IonButton onClick={handleAddTodo}>Add</IonButton>
-            </IonContent>
-        </IonPage>
-    )
-}
+  return (
+    <IonPage>
+      <IonContent>
+        <h1>Todo App</h1>
+        <IonList>{renderTodoList()}</IonList>
+        <IonInput
+          placeholder="Enter new Todo"
+          value={newTodo}
+          onIonInput={handleUserInput}
+        />
+        <IonButton onClick={handleAddTodo}>Add</IonButton>
+      </IonContent>
+    </IonPage>
+  );
+};
 
 export default Todo;
