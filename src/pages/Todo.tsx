1e098bc4990efa653a97f3073d22ba0073f548f3
James Attard <jamesat@mcast.org.mt>
2024-05-08 09:16:09 +0200

    Added the delete functionality

--- src/pages/Todo.tsx
+++ src/pages/Todo.tsx
@@ -28,21 +28,43 @@ const Todo: React.FC = () => {
 
   // Function will add the user's input to the todoList state array
   const handleAddTodo = () => {
-    const todoListArray = todoList;
-    todoListArray.push(newTodo);
-    setTodoList(todoListArray); // Update the todo list with the new item
+    const tempArray = todoList;
+    tempArray.push(newTodo);
+    setTodoList(tempArray); // Update the todo list with the new item
     setNewTodo(""); // Refresh the user's input field
 
     console.log("Todo list: ", todoList);
   };
 
+  const handleDelete = (indexToDelete: number) => {
+    // Create an intermediary array to store only the todo items I want to keep
+    // (and skip the one I want to delete)
+    const tempArray = [];
+    for (let i = 0; i < todoList.length; i++) {
+      if (i !== indexToDelete) {
+        tempArray.push(todoList[i]);
+      }
+    }
+
+    // Update the todoList state array with the final values of the finalTodoArray
+    // In other words, the todoList state array will contain all items except the one we just deleted
+    setTodoList(tempArray);
+  };
+
+  // Function will render the formatted version of the Todo list in an array called formattedArray
+  // 1) Initialize an empty array formattedArray
+  // 2) Loop through the state array todoList
+  // 3) For each element inside todoList, get the string todoList[i] (the todo item)
+  // 4) Inside the formattedArray, create an <IonItem> component with the previous string
+  // inside an <IonLabel> and followed by a delete <IonButton>
+  // 5) Return this formattedArray to the user
   const renderTodoList = () => {
     const formattedArray = [];
     for (let i = 0; i < todoList.length; i++) {
       formattedArray.push(
         <IonItem key={i}>
           <IonLabel>{todoList[i]}</IonLabel>
-          <IonButton>Delete</IonButton>
+          <IonButton onClick={() => handleDelete(i)}>Delete</IonButton>
         </IonItem>
       );
     }
